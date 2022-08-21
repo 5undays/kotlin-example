b5b0c40bc74f5e400f1022f25bf0f6a162f5ef80
SunYoung <baby_ash@naver.com>
2022-08-21 19:04:05 +0900

    init

---
+++ src/part1/datatype/datatype.kt
@@ -0,0 +1,11 @@
+package part1.datatype
+
+fun main() {
+    var name:String = "kildong"
+    var count = 3;
+    name = "dooly"
+
+    //var username2; //ERROR
+    var userName2 = "TEST"
+    println("Your name is ${name}, count ${count}")
+}
