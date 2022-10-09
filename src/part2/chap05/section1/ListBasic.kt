2868399f1bcafc422cd078eb40ca9befb8391171
SunYoung <baby_ash@naver.com>
2022-10-09 20:30:15 +0900

    컬렉션

---
+++ src/part2/chap05/section1/ListBasic.kt
@@ -0,0 +1,14 @@
+package part2.chap05.section1
+
+fun main() {
+    var numbers: List<Int> = listOf(1,2,3,4,5)
+    var names: List<String> = listOf("one", "two", "three")
+    var mixed = listOf(1,2,"3","4")
+
+    for (name in names) {
+        println(name)
+    }
+
+    for (num in numbers) print(num)
+    println()
+}
