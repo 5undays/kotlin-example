afd0cc74937c9e9bb2ef1f6324ff5e21c052b753
SunYoung <baby_ash@naver.com>
2022-11-24 20:47:04 +0900

    coroutines

--- src/part2/chap06/section2/BasicCoroutines.kt
+++ src/part2/chap06/section2/BasicCoroutines.kt
@@ -3,15 +3,22 @@ import kotlinx.coroutines.*
 
 fun main() { // 메인 스레드
     //doSomething() 사용불가
-    GlobalScope.launch {
-        delay(1000L) // 지연 함수
-        println("world")
-        doSomething()
+
+    // 하위의 자식이 완료될때까지 기다림
+    runBlocking {
+        val job = GlobalScope.launch {
+            delay(1000L) // 지연 함수
+            println("world")
+            doSomething()
+        }
+        println("Hello")
+        println("job : ${job.isCompleted}")
+        //job.join()
+        //Thread.sleep(2000L)
+        println("job : ${job.isActive}")
     }
-    println("Hello")
-    Thread.sleep(2000L)
 }
 
 suspend fun doSomething() {
-    print("DoSomething")
+    println("DoSomething")
 }
